3e4b9053b6e49da13e1e1443778697cffef2224f
Abdur Rahman <152638668+abdurrahman2028@users.noreply.github.com>
2024-09-24 13:03:54 +0600

    fast commit

---
+++ index.js
@@ -0,0 +1,49 @@
+
+
+
+// for (let i = 1; i <= 10; i++) {
+//     if (i % 2 == 0){
+//         console.log(i);
+//     }
+// }
+
+// let i = 1;
+// while (i <= 10){
+//     if (i % 2 == 0){
+//         console.log(i);
+//     }
+// i++;
+// }
+
+// let friend = ["John", "Jane", "Bob", "Alice"];
+// for (let i = 0; i < friend.length; i++){
+//         console.log(friend[i]);
+// }
+
+
+// for (let i = 0; i <= 20; i++){
+//     if(i % 2 === 0){
+//         if(i === 10){
+//             break;
+//         }
+//         console.log(i); 
+//     }
+// }
+
+
+// let arr = ['rokon', 'riya', 'raisa', 'jahid', 'soliman', 'tanisa'];
+// for (i = 0; i < arr.length; i++){
+//     console.log(arr[i]);
+// }
+
+
+
+let number = [24, 56, 78, 90, 12, 34, 67, 89, 45, 67];
+for (i = 0; i < number.length; i++){
+    console.log(number[i] * 2);
+}
+
+
+
+
+
